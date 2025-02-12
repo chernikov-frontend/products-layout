fe741db7470944919f7098bb35dd97ec71fdb8e6
chernikov-frontend <chernikov-frontend@yandex.ru>
2025-02-12 16:58:20 +0400

    +

--- src/components/IconSwitch.jsx
+++ src/components/IconSwitch.jsx
@@ -1,7 +1,7 @@
 const IconSwitch = ({ icon, onSwitch }) => {
     return (
         <button className="icon-switch" onClick={onSwitch}>
-            <img src={`/icons/${icon}.svg`} alt="switch icon" />
+            <img src={`${import.meta.env.BASE_URL}icons/${icon}.svg`} alt="switch icon" />
         </button>
     );
 };
